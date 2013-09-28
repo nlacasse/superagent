09d2410035da6f8786985ad1e9903bcdf3552e5b
Nicolas LaCasse <nicolas.lacasse@gmail.com>
2013-09-27 19:00:20 -0700

    hacks for webworkers

--- superagent.js
+++ superagent.js
@@ -1,4 +1,4 @@
-;(function(){
+(function(){
 
 /**
  * Require the given path.
@@ -464,7 +464,7 @@ function getXHR() {
     try { return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } catch(e) {}
     try { return new ActiveXObject('Msxml2.XMLHTTP'); } catch(e) {}
   }
-  return false;
+  return new XMLHttpRequest;
 }
 
 /**
